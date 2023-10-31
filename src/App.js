097a517626374af730c16be51c6989c8f964eb81
Harikrishnan <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>
2023-10-31 15:34:39 +0530

    todoApp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import  Todo from './Components/Todo'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1><Todo/></h1>
+    )
+  }
+}
+
+export default App;
